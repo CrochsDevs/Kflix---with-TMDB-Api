c656e8193998c1329aecde1c856b318849eb2da1
immortal1sm <solomonallan27@gmail.com>
2026-04-07 17:42:22 +0800

    Fix paths in bootstrap/app.php, fix artisan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+        //
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,10 +4,10 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__, 2))
     ->withRouting(
-        web: __DIR__.'/routes/web.php',
-        commands: __DIR__.'/routes/console.php',
+        web: dirname(__DIR__, 2).'/routes/web.php',
+        commands: dirname(__DIR__, 2).'/routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
